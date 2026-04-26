5f1893939690feb83b63abb336d6f01db7b19a21
Atha2403 <hilalazka02@gmail.com>
2026-04-26 13:50:51 +0700

    tugas routing

---
+++ resources/views/intro.blade.php
@@ -0,0 +1,79 @@
+<html>
+
+<head>
+    <title>5026241145 - Athallah Hilman Hilalazka</title>
+<style>
+        body {
+            background-color: #e6f2ff;
+            margin-top: 50px;
+            margin-bottom: 100px;
+            margin-right: 80px;
+            margin-left: 80px;
+        }
+
+        p {
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+
+        #bikinjaditengah {
+            text-align: center;
+            font-size: xx-small;
+        }
+
+        .bikinjaditengah2 {
+            text-align: center;
+            font-size: xx-small;
+        }
+
+        .judulberita {
+            text-align: center;
+            font-size: large;
+            color: rgb(55, 95,223);
+        }
+
+        .judulgambar {
+            text-align: center;
+            font-size: x-small;
+        }
+
+        .footerbold {
+            font-weight: bold;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+
+        a:link{
+            text-decoration: none;
+            color: rgb(55, 95,223);
+        }
+        a:hover{
+            text-decoration: none;
+            color: rgb(55, 95,223);
+        }
+        a:visited{
+            text-decoration: none;
+            color: rgb(55, 95,223);
+        }
+
+        .formatgambar{
+            border-radius: 15px;
+            box-shadow: 0px 4px 10px 2px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Selamat Datang di ITS</h1>
+    <p>
+        This paragraph
+        contains a lot of lines
+        in the source code,
+        <br>but the browser
+        ignores it.
+    </p>
+    <p>
+        <img src="https://www.its.ac.id/news/wp-content/uploads/sites/2/2026/01/Tim-Antasena-ITS-bersama-kendaraan-Prototype-Hydrogen-Fuel-Cell-di-lintasan-Shell-Eco-marathon-Asia-Pacific-and-the-Middle-East-2026-Qatar-1.jpg"
+        width="600px">
+    </p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,29 @@ Route::get('pert5', function () {
 	return view('pertemuan5');
 });
 
+Route::get('pert1', function () {
+	return view('intro');
+});
+
+Route::get('pert2', function () {
+	return view('news1');
+});
+
+Route::get('pert3', function () {
+	return view('responsive');
+});
+
+Route::get('task-tm3', function () {
+	return view('task-tm3');
+});
+
+Route::get('pert4', function () {
+	return view('145');
+});
+
+Route::get('linktree', function () {
+	return view('linktree');
+});
+
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
